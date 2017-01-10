1e5422337c5b05ebcb2f4df02cee7b03b05294f8
Jacek Kopecky <jacek@jacek.cz>
2017-01-10 23:39:07 +0000

    first cut of web sockets tests, doesn't mention html client

---
+++ README.md
@@ -0,0 +1,55 @@
+ws_ws
+=====
+
+An intro to web sockets.
+
+
+Running Tests
+-------------
+
+We continue to use QUnit to define tests that you should attempt to complete.  
+The automated tests are with `npm` on the command line; part of the task is to
+create a web page that you'll have to test by looking if it's doing the right
+thing.
+
+The usual steps to install the source code and the test framework and then run
+the tests from the command line:
+
+1. To download the code, either use git (the simplest option):
+
+  ```bash
+  git clone https://github.com/portsoc/ws_ws.git
+  cd ws_ws
+  ```
+  or download and unpack the [zip](https://github.com/portsoc/ws_ws/archive/master.zip)
+  which on linux can be achieved using
+  ```bash
+  wget https://github.com/portsoc/ws_ws/archive/master.zip
+  ```
+  then
+  ```bash
+  unzip master.zip
+  cd ws_ws-master
+  ```
+
+2. To download the QUnit files (and any libraries it uses, which you need to do before the first run of tests, but just the once) type:
+
+  ```bash
+  npm install
+  ```
+
+3. Run the tests by typing:
+
+  ```bash
+  npm test
+  ```
+
+4. **Inside `test.js` you will find helpful comments that tell you what the tests expect.**
+
+Git: A recommendation
+----------------------
+If at all possible, we recommend you use git to download code rather than zips
+of a repository.  This is preferable because if the repo is updated, then
+syncing those changes requires just one command (`git pull`) and usually any
+merging can be done automatically.  Git is very powerful and we heartily
+encourages you to become familiar with it.
---
+++ test.js
@@ -0,0 +1,115 @@
+'use strict';
+
+require('./logger').setupLogging(QUnit, test);
+
+var fs = require('fs');
+var WebSocket = require('ws');
+
+/**
+ * Simple WebSocket coordinates generator.
+ *
+ * Create a server (in `worksheet/server.js`) that accepts WebSocket
+ * connections on port 8080 and twice a second (every 500ms) generates
+ * a pair of XY coordinates and sends them to every client currently connected.
+ *
+ * Both coordinates must be in the range 0..1000, and an example might
+ * look like this: { x: 345, y: 42 }
+ */
+test(
+  "`server.js` should exist in `worksheet/`",
+  function () {
+    try {
+      fs.accessSync('worksheet/server.js', fs.F_OK);
+      ok(true, "server.js created");
+    } catch (e) {
+      ok(false, "worksheet/server.js is missing - please create it");
+    }
+  }
+);
+
+test(
+  "server should accept web socket connections on 8080",
+  function () {
+    // start the server
+    console.log('starting server, if you see EADDRINUSE errors, something is blocking port 8080.');
+    require('./worksheet/server');
+    stop(); // stop qunit so it waits for asynchrony
+
+    const NUM = 3; // how many connections we'll try
+    const DELAY = 5000;
+    const MSG_PER_SEC = 2;
+    const MSG_DELAY = 1000 / MSG_PER_SEC;
+
+    const EXP = Math.round(DELAY / 1000 * MSG_PER_SEC);
+    const D = 1; // expecting EXPECTED_MSG ± D messages from each connection
+
+    const wss = [];
+    const opened = [];
+    const received = [];
+
+    let lastTime = 0;
+    let lastMsg = null;
+
+    let jsonErrMax = 2;
+    let sameErrMax = 3;
+    let diffErrMax = 2;
+
+    // give the server time to start
+    setTimeout(() => {
+
+      for (let i=0; i<NUM; i++) {
+        wss[i] = new WebSocket('ws://localhost:8080/path');
+
+        wss[i].on('open', function () {
+          opened[i] = true;
+          console.log(`opened connection ${i}`);
+        });
+
+        wss[i].on('message', function(data, flags) {
+          received[i] = (received[i] || 0) + 1;
+          console.log(`received ${data} on connection ${i}`);
+
+          try {
+            var coords = JSON.parse(data);
+
+          } catch (e) {
+            if (jsonErrMax-- > 0) ok(false, 'coordinates should be sent as JSON strings');
+          }
+
+          let now = Date.now();
+          if ((now - lastTime < (MSG_DELAY*.9)) && (data != lastMsg) && (diffErrMax-- > 0)) {
+            equal(data, lastMsg, `coordinates received within ${MSG_DELAY*.9}ms should be the same`);
+          } else
+          if ((now - lastTime >= (MSG_DELAY*.9)) && (data == lastMsg) && (sameErrMax-- > 0)) {
+            notEqual(data, lastMsg, `subsequent coordinates should not be the same`);
+          }
+          lastTime = now;
+          lastMsg = data;
+        });
+      }
+
+    }, 100);
+
+    // check the results
+    setTimeout(function() {
+
+      let successfullyOpened = 0;
+      for (let i=0; i<NUM; i++) {
+        if (opened[i]) successfullyOpened++;
+      }
+      equal(successfullyOpened, NUM, `expecting all ${NUM} connections to successfully open`);
+
+      for (let i=0; i<NUM; i++) {
+        ok((received[i] >= EXP - D) && (received[i] <= EXP + D), `expecting between ${EXP - D} and ${EXP + D} messages from connection ${i}, got ${received[i]}`);
+      }
+
+      // close all the connections so we don't get any more messages
+      for (let i = 0; i<NUM; i++) {
+        wss[i].close();
+      }
+
+      // start qunit again after all the asynchrony
+      start();
+    }, DELAY);
+  }
+)
