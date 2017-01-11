c4e0844fd091956418512260b2f1fa758275fcb6
Jacek Kopecky <jacek@jacek.cz>
2017-01-11 00:31:00 +0000

    fancy-pants walking paths

--- worksheet/server.js
+++ worksheet/server.js
@@ -18,12 +18,39 @@ wss.on('connection', (ws) => {
   });
 });
 
+
 setInterval(() => {
-  var newCoords = JSON.stringify({x: Math.round(Math.random()*100), y: Math.round(Math.random()*100)});
+  var coords = JSON.stringify(newCoords());
   wss.clients.forEach( (client) => {
-    client.send(newCoords, (e) => {});
+    client.send(coords, (e) => {});
   });
 }, 500);
 
 
 server.listen(8080, function () { console.log('Server started.'); });
+
+
+
+// fancy-pants walking coords
+
+function r() { return Math.random()*30+5; }
+
+let x = 10;
+let y = 10;
+let dx = r();
+let dy = r();
+
+function newCoords () {
+  x+=dx;
+  y+=dy;
+
+  if (x>=100) { x = 100; dx = -r(); }
+  if (y>=100) { y = 100; dy = -r(); }
+  if (x<=0)   { x = 0;   dx = r(); }
+  if (y<=0)   { y = 0;   dy = r(); }
+
+  if (Math.random() < .01) dx = -dx;
+  if (Math.random() < .01) dy = -dy;
+
+  return {x: x, y: y};
+}
